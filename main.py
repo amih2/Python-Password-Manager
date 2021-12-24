691d7acfb7344bfa377706ba086c3c8553caf2c3
amih2 <96546596+amih2@users.noreply.github.com>
2021-12-24 23:57:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from modules.menu import Menu
 
 menu = Menu()
 menu.check_for_root()
-filename = 'database.json'
+database = 'database.json'
 key = 'pass.key'
 
 
 choice = menu.menu()
 while choice != 'Q':
   if choice == '1':
-    menu.create(filename, key)
+    menu.create(database, key)
   if choice == '2':
-    menu.retrieve_data(filename, key)
+    menu.retrieve_data(database, key)
   if choice == '3':
-    menu.delete(filename, key)
+    menu.delete(database, key)
   if choice == 'q' or choice == 'Q':
         exit()
   else:
